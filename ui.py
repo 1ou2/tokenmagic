50bb179dd3682dc4c69e51ddd530d953880734d6
Gabriel <gabriel@or.com>
2025-08-26 14:54:21 +0200

    cleanup

--- ui.py
+++ ui.py
@@ -1,30 +1,19 @@
 import gradio as gr
 from openai import AzureOpenAI
 from dotenv import load_dotenv
-
 import os
 
-load_dotenv()
-key = os.getenv("OPENAI_KEY")
-endpoint = os.getenv("OPENAI_ENDPOINT")
-key = os.getenv("GPT4_KEY")
-endpoint = os.getenv("GPT4_ENDPOINT")
-client = AzureOpenAI(
-    api_key=key,  
-    api_version="2024-02-01",
-    azure_endpoint = endpoint
-)
 
-def greet(name, intensity):
-    return "Hello, " + name + "!" * int(intensity)
 
-def get_completion(prompt, model="gpt-4"):
+def get_completion(prompt):
     messages = [{"role": "user", "content": prompt}]
     print(f"Prompt: {prompt}")
+    
     response = client.chat.completions.create(
         model=model,
         messages=messages
     )
+   
     completion = response.choices[0].message.content
     print(f"Completion: {completion}")
     return completion
@@ -32,6 +21,24 @@ def get_completion(prompt, model="gpt-4"):
 def repeat_after_me(text):
     prompt = "Repeat after me :" + text
     return prompt, get_completion(prompt)
+
+
+load_dotenv()
+model = "gpt-4"
+if model == "gpt-4":
+    key = os.getenv("GPT4_KEY")
+    endpoint = os.getenv("GPT4_ENDPOINT")
+elif model == "gpt-3":
+    key = os.getenv("GPT3_KEY")
+    endpoint = os.getenv("GPT3_ENDPOINT")
+else:
+    raise ValueError("Invalid model")
+
+client = AzureOpenAI(
+    api_key=key,  
+    api_version="2024-02-01",
+    azure_endpoint = endpoint
+)
 
 demo = gr.Interface(
     title="Repeat after me",
